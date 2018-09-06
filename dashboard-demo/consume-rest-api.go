e97111e7b4155b2b58625d2e5ca64bd9e971b0c1
sumantabose <sumantabose@gmail.com>
2018-09-07 04:32:59 +0800

    added consume-rest-api.go

---
+++ dashboard-demo/consume-rest-api.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+
+type Product struct {
+    Name string `json:"Name"`
+    SerialNo int `json:"SerialNo"`
+    CodeNo int `json:"CodeNo"`
+    Location int `json:"Location"`
+}
+
+func main() {
+	response, err := http.Get("http://localhost:8090/next")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject Product
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject)
+	fmt.Println(responseObject.Name)
+	fmt.Println(responseObject.SerialNo)
+	fmt.Println(responseObject.CodeNo)
+	fmt.Println(responseObject.Location)
+
+	///////
+
+	response, err = http.Get("http://localhost:8090/next/5")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseData, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject2 []Product
+	json.Unmarshal(responseData, &responseObject2)
+
+	fmt.Println(responseObject2)
+}
+
