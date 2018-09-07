0e2a1a6d1bdecb15945c4ab96e923f8a46f2c39a
Sumanta Bose <sumantabose@gmail.com>
2018-09-07 16:28:35 +0800

    updated peer

--- node-discovery/peer.go
+++ node-discovery/peer.go
@@ -1,125 +1,109 @@
+/* README
+
+Written by Sumanta Bose, 7 Sept 2018
+
+*/
+
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"bytes"
+    "log"
+    "time"
+    "bytes"
+    "net/http"
+    "math/rand"
+    "io/ioutil"
+    "encoding/json"
+
+    "github.com/davecgh/go-spew/spew"
 )
 
+///// GLOBAL CONSTS & VARIABLES 
 
-type Product struct {
-    Name string `json:"Name"`
-    SerialNo int `json:"SerialNo"`
-    CodeNo int `json:"CodeNo"`
-    Location int `json:"Location"`
+const (
+    bootstrapperPort = "51000"
+)
+
+type Peer struct {
+    PeerAddress string `json:"PeerAddress"`
+}
+
+var thisPeer Peer
+var ListOfPeers []Peer
+
+/////
+
+///// LIST OF FUNCTIONS
+
+func init() {
+    log.SetFlags(log.Lshortfile)
+    thisPeer = Peer {PeerAddress : genRandString(15)}
 }
 
 func main() {
-
-	fmt.Println("\nGET\n")
-	response, err := http.Get("http://localhost:8090/next")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var responseObject Product
-	json.Unmarshal(responseData, &responseObject)
-
-	fmt.Println(responseObject)
-	fmt.Println(responseObject.Name)
-	fmt.Println(responseObject.SerialNo)
-	fmt.Println(responseObject.CodeNo)
-	fmt.Println(responseObject.Location)
-
-	///////
-	
-	fmt.Println("\nGET LOOP\n")
-	response, err = http.Get("http://localhost:8090/next/5")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-	responseData, err = ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var responseObject2 []Product
-	json.Unmarshal(responseData, &responseObject2)
-
-	fmt.Println(responseObject2)
-
-	///////
-	fmt.Println("\nPOST I\n")
-
-	sampleProduct := Product {
-		Name: "XBOX",
-		SerialNo: 25,
-		CodeNo: 9123,
-		Location: 2,
-	}
-
-	jsonValue, err := json.Marshal(sampleProduct)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	url := "http://localhost:8090/post"
-
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
-    req.Header.Set("Content-Type", "application/json")
-
-    client := &http.Client{}
-    response, err = client.Do(req)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer response.Body.Close()
-
-    fmt.Println("response Status:", response.Status)
-    fmt.Println("response Headers:", response.Header)
-    body, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("response Body:", string(body))
-
-    ///////
-
-    fmt.Println("\nPOST II\n")
-
-    sampleProduct2 := Product {
-		Name: "YBOX",
-		SerialNo: 35,
-		CodeNo: 8123,
-		Location: 3,
-	}
-
-	jsonValue2, err := json.Marshal(sampleProduct2)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	response, err = http.Post(url, "application/json", bytes.NewBuffer(jsonValue2))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-    fmt.Println("response Status:", response.Status)
-    fmt.Println("response Headers:", response.Header)
-    body, err = ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("response Body:", string(body))
+	queryP2PList()
+	joinP2PNet()
+	queryP2PList()
 }
 
+
+func queryP2PList() { // Query the list of peers in the P2P Network from the Bootstrapper
+	log.Println("Querying list of peers")
+	
+	response, err := http.Get("http://localhost:" + bootstrapperPort + "/query-p2p-list")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	//var responseObject ListOfPeers
+	json.Unmarshal(responseData, &ListOfPeers)
+	spew.Dump(ListOfPeers)
+}
+
+func joinP2PNet() { // Join the P2P Network by adding THIS peer's address to list of peers with Bootstrapper
+	log.Println("Joining P2P network")
+
+	jsonValue, err := json.Marshal(thisPeer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	url := "http://localhost:" + bootstrapperPort + "/join-p2p-net"
+	response, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+    log.Println("response Status:", response.Status)
+    log.Println("response Headers:", response.Header)
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    log.Println("response Body:", string(body))
+}
+
+///// LIST OF HELPER FUNCTIONS
+
+func genRandString(n int) string { // generate Random String of length 'n'
+    var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    val := make([]rune, n)
+    for i := range val {
+        myRandSource := rand.NewSource(time.Now().UnixNano())
+        myRand := rand.New(myRandSource)
+        val[i] = letterRunes[myRand.Intn(len(letterRunes))]
+    }
+    return string(val)
+}
