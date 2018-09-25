9dd109b3f1ad2a7986a5a90370f9b065105687aa
Sumanta Bose <sumantabose@gmail.com>
2018-09-25 21:17:38 +0800

    Added 5-p2p-block-struct/

---
+++ 5-p2p-block-struct/defs.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"flag"
+	"math/rand"
+	"time"
+	gonet "net"
+	host "github.com/libp2p/go-libp2p-host"
+)
+
+///// FLAG & VARIABLES
+
+	//var listenF *int
+	//var target *string
+	var secio *bool
+	var verbose *bool
+	var seed *int64
+	var dataDir *string // data directory prefix where the gob files are stored
+
+	var ha host.Host
+
+/////
+
+// Block represents each 'item' in the blockchain
+type RawMaterialTransaction struct {
+	Name string
+}
+
+type DeliveryTransaction struct {
+	Age int
+}
+
+type Block struct {
+	Index     int
+	Timestamp string
+	TxnType int
+	TxnPayload interface{}
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+// Blockchain is a series of validated Blocks
+var Blockchain []Block
+
+// Message takes incoming JSON payload for writing heart rate
+type Message struct {
+	BPM int
+}
+
+type newTarget_json struct {
+	NewTarget string
+}
+
+var mutex = &sync.Mutex{}
+
+////////  HELPER FUNCTIONS
+
+func readFlags() {
+	// Parse options from the command line
+	//listenF = flag.Int("l", 0, "wait for incoming connections")
+	//target = flag.String("d", "", "target peer to dial")
+	secio = flag.Bool("secio", true, "enable secio")	
+	verbose = flag.Bool("v", false, "enable verbose")
+	seed = flag.Int64("seed", 0, "set random seed for id generation")
+	dataDir = flag.String("data", "data", "pathname of data directory")
+	flag.Parse()
+}
+
+func  GetMyIP() string {
+	var MyIP string
+
+	conn, err := gonet.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+	   log.Fatalln(err)
+	} else {
+		localAddr := conn.LocalAddr().(*gonet.UDPAddr)
+		MyIP = localAddr.IP.String()
+	}
+	return MyIP
+}
+
+func genRandInt(n int) int {
+    myRandSource := rand.NewSource(time.Now().UnixNano())
+   	myRand := rand.New(myRandSource)
+   	val := myRand.Intn(n)
+   	return val
+}
---
+++ 5-p2p-block-struct/mux.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"encoding/json"
+	"time"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"net/http"
+	"io"
+	"log"
+	"strconv"
+)
+
+
+// web server
+func muxServer() error {
+	mux := makeMuxRouter()
+	//httpPort := os.Getenv("PORT")
+	log.Println("HTTP Server Listening on port :", peerProfile.PeerPort) // peerProfile.PeerPort in peer-manager.go
+	s := &http.Server{
+		Addr:           ":" + strconv.Itoa(peerProfile.PeerPort),
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// create handlers
+func makeMuxRouter() http.Handler {
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/bpm/", handleBPMWriteBlock).Methods("POST")
+	muxRouter.HandleFunc("/raw/", handleRawMaterialTxnWriteBlock).Methods("POST")
+	muxRouter.HandleFunc("/delivery/", handleDeliveryTxnWriteBlock).Methods("POST")
+	muxRouter.HandleFunc("/connect", handleConnect).Methods("POST")
+	return muxRouter
+}
+
+// write blockchain when we receive an http request
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	mutex.Unlock()
+	
+	io.WriteString(w, string(bytes))
+}
+
+// takes JSON payload as an input for heart rate (BPM)
+func handleBPMWriteBlock(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var m Message
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	mutex.Lock()
+	newBlock := generateBlock(Blockchain[len(Blockchain)-1], m.BPM, "", 0)
+	mutex.Unlock()
+
+	if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+		mutex.Lock()
+		Blockchain = append(Blockchain, newBlock)
+		mutex.Unlock()
+		spew.Dump(Blockchain)
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+}
+
+// takes JSON payload as an input for Raw Material Transaction
+func handleRawMaterialTxnWriteBlock(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var rmTxn RawMaterialTransaction
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&rmTxn); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	mutex.Lock()
+	newBlock := generateBlock(Blockchain[len(Blockchain)-1], 0, rmTxn, 1)
+	mutex.Unlock()
+
+	if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+		mutex.Lock()
+		Blockchain = append(Blockchain, newBlock)
+		mutex.Unlock()
+		spew.Dump(Blockchain)
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+}
+
+// takes JSON payload as an input for Delivery Transaction
+func handleDeliveryTxnWriteBlock(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var delTxn DeliveryTransaction
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&delTxn); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	mutex.Lock()
+	newBlock := generateBlock(Blockchain[len(Blockchain)-1], 0, delTxn, 2)
+	mutex.Unlock()
+
+	if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+		mutex.Lock()
+		Blockchain = append(Blockchain, newBlock)
+		mutex.Unlock()
+		spew.Dump(Blockchain)
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+}
+
+func handleConnect(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var m newTarget_json
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		log.Println(err)
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	log.Println("mux NewTarget =", m.NewTarget)
+	connect2Target(m.NewTarget)
+	respondWithJSON(w, r, http.StatusCreated, m.NewTarget)
+}
+
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
